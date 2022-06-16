52651e2e797dbb279932b0aa831be49ceb704502
Franco Melgar <francomelgar4@gmail.com>
2022-06-16 17:11:37 -0500

    adding pdfroute, controllers

---
+++ src/routes/pdf/controller.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import template from '../../utils/templatepdf'
+import pdf, { CreateOptions } from 'html-pdf'
+import { Request, Response } from 'express'
+
+const options: CreateOptions = {
+  format: 'A4',
+  orientation: 'portrait',
+}
+
+const createPdf = (req: Request, res: Response) => {
+  const date = new Date().toISOString()
+  const datereg = date.slice(0, 13)
+  const sec = new Date().getMilliseconds()
+  //   console.log(date.length)
+  console.log(sec)
+  const { product, category, ubication, price, user } = req.body
+
+  console.log(req.body)
+  pdf
+    .create(template(product, category, ubication, price, user), options)
+    .toFile(`./src/pdfsgenerated/${datereg}${sec}_test.pdf`, (err, res) => {
+      // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+      err && console.log(err)
+      return console.log(res)
+    })
+  return res.json({ filename: `${datereg}${sec}_test.pdf` })
+}
+
+const getPdf = (_req: Request, res: Response) => {
+  res.send('holamundo')
+}
+
+export { createPdf, getPdf }
